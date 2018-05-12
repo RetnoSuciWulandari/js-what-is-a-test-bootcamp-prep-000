4ab5436631702b8948802f71032bb36e007e5f6b
Retno Suci Wulandari <retnosw389@gmail.com>
2018-05-12 16:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
+var nickName = "Joe"
 var height = 74
-var message = `${name} is ${height} inches tall`
+var message = `${nickName} is ${height} inches tall`
 
-module.exports = {name, height, message}
+module.exports = {nickName, height, message}
